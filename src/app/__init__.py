eb32df446f797dfb8b8c035ac7bae063c7a58b8d
Hiển Phạm <hootpy@Hiens-MacBook-Air.local>
2024-05-06 16:53:32 +0930

    fix: add cors middleware

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from app.setup import middlewares
+
 
 def create_app() -> FastAPI:
     from app.apis.routes import router as api_router
@@ -8,6 +10,7 @@ def create_app() -> FastAPI:
         title="HelloSpeak BE",
         version="0.1.0",
         description="Backend for HelloSpeak",
+        middleware=middlewares,
     )
 
     app.include_router(api_router, prefix="/api")
