0878f80701c8f9af60d7f9e768503e41562f04a6
Hiển Phạm <hootpy@Hiens-MacBook-Air.local>
2024-05-06 17:03:18 +0930

    fix: add cors middleware

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-from app.setup import middlewares
+from app.config.settings import SETTING
 
 
 def create_app() -> FastAPI:
@@ -10,9 +11,14 @@ def create_app() -> FastAPI:
         title="HelloSpeak BE",
         version="0.1.0",
         description="Backend for HelloSpeak",
-        middleware=middlewares,
     )
-
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=SETTING.ALLOW_ORIGINS,
+        allow_credentials=SETTING.ALLOW_CREDENTIALS,
+        allow_methods=SETTING.ALLOW_METHODS,
+        allow_headers=SETTING.ALLOW_HEADERS,
+    )
     app.include_router(api_router, prefix="/api")
 
     return app
